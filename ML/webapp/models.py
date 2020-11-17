9db1fd2d2d82d1edc7e9b1676f3a9efae91a7e6e
sirox548 <55093250+sirox548@users.noreply.github.com>
2020-11-17 09:33:21 -0500

    .

--- ML/webapp/models.py
+++ ML/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Description(models.Model):
+    job_list = (('Java Developer', 'Java Developer'), ('Sales Manager', 'Sales Manager'))
+    title = models.CharField(max_length=100, null=True)
+    jobs = models.CharField(max_length=100, choices=job_list, null=True)
+    job_description = models.TextField(max_length=500,  null=True)
+
+    def __str__(self):
+        return self.title
+
--- ML/webapp/views.py
+++ ML/webapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Description
 
 
 def home(request):
-    return render(request, 'webapp/home.html')
+    Jobs = Description.objects.all()
+    context = {'Jobs': Jobs}
+    return render(request, 'webapp/home.html', context)
 
 
 def results(request):
